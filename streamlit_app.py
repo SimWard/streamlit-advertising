930ae4f9828d52be18f2d6ccf7a2b5761a6a2524
Ryan Ward <wardyryan1@gmail.com>
2021-04-11 10:42:57 +1000

    Changes for readability

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,24 +18,34 @@ st.write("")
 st.write("")
 
 st.write("""
-        For this exploration, we'll use an advertising dataset made famous by Gareth James, \
-        Daniela Witten, Trevor Hastie and Rob Tibshirani in their terrific book - an  \
-        Introduction to Statistical Learning.\n
-        It contains the local advertising budget (in $'000) for a company at \
-        different locations and the number of sales made of a product (in thousands of units).\n
-        We'll start with a quick exploration before conducting a type of machine learning \
-        called regression, then we can make some predictions.
+        Marketers often have trouble predicting in advance where to get the best value for their \
+        limited budgets.\n
+        Data can help here, and it is easier than you think.
         """
          )
+
+st.write("")
+
+st.write("""
+        For this exploration, we'll use an example advertising dataset from a terrific book - \
+        *An Introduction to Statistical Learning*.\n
+        It contains the advertising budgets (in $'000) for a company at \
+        different locations and the number of sales made of a product (in thousands of units).\n
+        We'll start with a brief exploration before conducting a type of machine learning \
+        called regression. We can then make some predictions.
+        """
+         )
+
+st.write('## Into the analysis!')
 
 url = "https://www.statlearning.com/s/Advertising.csv"
 
 
 @st.cache
 def load_data(url):
-  data = pd.read_csv(url, index_col=0)
-  data.columns = ['TV', 'Radio', 'Newspaper', 'Sales']
-  return data
+    data = pd.read_csv(url, index_col=0)
+    data.columns = ['TV', 'Radio', 'Newspaper', 'Sales']
+    return data
 
 
 data = load_data(url)
@@ -44,8 +54,8 @@ data = load_data(url)
 # data_load_state.text('Advertising data imported!')
 
 if st.checkbox('Show raw data'):
-  st.write('### Raw data')
-  st.write(data)
+    st.write('### Raw data')
+    st.write(data)
 
 st.write('### Summary statistics')
 st.dataframe(data.describe())
@@ -65,14 +75,14 @@ st.write("")
 
 
 def plot_scatter(x, y):
-  st.altair_chart(alt.Chart(data)
-                  .mark_circle(size=60)
-                  .encode(
-                      x=x,
-                      y=y,
-                      tooltip=['TV', 'Radio', 'Newspaper', 'Sales'])
-                  .properties(title=f'{x} budget vs {y}')
-                  )
+    st.altair_chart(alt.Chart(data)
+                    .mark_circle(size=60)
+                    .encode(
+                        x=x,
+                        y=y,
+                        tooltip=['TV', 'Radio', 'Newspaper', 'Sales'])
+                    .properties(title=f'{x} budget vs {y}')
+                    )
 
 
 plot_scatter('TV', 'Sales')
@@ -81,9 +91,9 @@ plot_scatter('Newspaper', 'Sales')
 
 st.write("""
         #### What do the charts tell us?\n
-        * **TV budget** increases reliably with sales but plateaus at the higher values\n
-        * **Radio budget** also increases with sales but not as strong as TV'\n
-        * **Newspaper budget** shows the weakest relationship with sales'\n
+        * **TV budget** _increases reliably_ with sales but plateaus at the higher values\n
+        * **Radio budget** also increases with sales but _not as strong_ as TV\n
+        * **Newspaper budget** shows the _weakest relationship_ with sales\n
         We can explore this further with a type of machine learning called regression.
         """)
 
@@ -144,9 +154,9 @@ rmse = round(mse**0.5, 2)
 
 st.write('### How do we know if our model performed well?')
 st.write('')
-st.write(f'The Square Root of the Mean Squared Error (rmse) is one metric we can use. \
-           Here, the rmse was {rmse}.  \n \
-           This can be interpreted that in general the sales prediction will be out \
+st.write(f'The Square Root of the Mean Squared Error (rmse) is one metric we can use which helps \
+           measure how accurate our prediction is. Here, the rmse was {rmse}.  \n\
+           This means that in general the sales prediction will be out \
            by {int(rmse*1000)} units.')
 st.write('#### Is this good?')
 st.write('It depends! For some medical diagnosis model you might need to be far more accurate, but in a \
@@ -163,18 +173,18 @@ radio_value = st.slider('Radio budget (in $\'000)', min_value=0, max_value=500, 
 newspaper_value = st.slider('Newspaper budget (in $\'000)', min_value=0, max_value=500, value=0)
 
 if st.button('How many things am I going to sell?'):
-  # st.progress(progress_variable_1_to_100)
-  st.spinner()
-  with st.spinner(text='Thinking really hard...'):
-    sleep(2)
-    st.success('Prediction complete')
-  single_prediction = reg.predict([[TV_value, radio_value, newspaper_value]])
-  st.write(f'The model predicts you\'ll sell {int(single_prediction[0])}k units from a  \
+    # st.progress(progress_variable_1_to_100)
+    st.spinner()
+    with st.spinner(text='Thinking really hard...'):
+        sleep(2)
+        st.success('Prediction complete')
+    single_prediction = reg.predict([[TV_value, radio_value, newspaper_value]])
+    st.write(f'The model predicts you\'ll sell {int(single_prediction[0])}k units from a  \
                combined advertising budget of TV: ${TV_value}k, Radio: ${radio_value}k and \
                Newspaper: ${newspaper_value}k.')
 
-  st.write('Tinker with the inputs and see if you can find an optimal budget - making \
+    st.write('Tinker with the inputs and see if you can find an optimal budget - making \
               the most sales for the least cost!')
-  st.write('')
-  st.write('#### Feel free to reach out to me at ryan@fromlawtodata.com if you have \
-                   any questions.')
+    st.write('')
+    st.write('Feel free to reach out to me at ryan@fromlawtodata.com if you have \
+              any questions.')
